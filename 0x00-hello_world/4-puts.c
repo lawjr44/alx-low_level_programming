608a1e649395ef1eb1585d8c5962f094d55661e8
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-07 15:32:16 -0700

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * main - this is the main function
- * return; is always 0
+ *
+ * Return: always 0
  */
 
-int main (void)
+int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
-	return 0;
+return (0);
 }
 
 
