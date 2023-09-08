b160f01dfba37ebbda0b746ce3c3ed6987f424a3
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-08 14:53:08 -0700

    alphABETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: 0 indicates successful program execution
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (i = 'A' ; i <= 'Z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
