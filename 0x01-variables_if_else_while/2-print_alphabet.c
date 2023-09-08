2af17477f76837753bc0cb42a5c1c80415a3df9f
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-08 14:28:38 -0700

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: 0 indicates succesful program execution
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = '4' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
