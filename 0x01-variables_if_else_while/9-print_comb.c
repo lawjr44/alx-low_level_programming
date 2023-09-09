21fc82f437054c46343bf318607d005796ff7c50
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-08 18:37:51 -0700

    combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point of the program
+ *
+ * Return: 0 indicates successful program execution
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
