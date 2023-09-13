e7fc850f84b208e94336bc08614ce755ec1408ec
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-13 09:54:50 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry of the program
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
