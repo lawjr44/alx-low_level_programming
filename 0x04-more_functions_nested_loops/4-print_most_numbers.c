9b677a700859dcc978ea3d0555a22dccfa5b8f1f
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-14 15:35:19 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,14 @@
 
 void print_most_numbers(void)
 {
-	char c;
+	int i;
 
-	for (c = '0'; c <= '9'; c++)
+	for (i = 0; i < 10; i++)
 	{
-	if (!(c == '2' || c == '4'))
-	_putchar(c);
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
 	}
 	_putchar('\n');
 }
+
 
