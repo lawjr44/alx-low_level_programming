2103e64977b57b3e09d27694a249e376428cfc7b
lawjr44 <lawsonhazardjunior@gmail.com>
2023-09-18 15:28:21 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - this prints a string in reverse
+ * @s: parameter string
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+	for (i = count - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
+
