6a70307d1cdb9693a86e783177288ef14f3cb39c
lawjr44 <lawsonhazardjunior@gmail.com>
2023-10-11 08:57:43 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,20 +4,27 @@
  * print_name - Prints the name `name` using the function `f`
  * @name: A pointer to the name to be printed
  * @f: A pointer to the function to be used to print the name
- * Return: nothing
+ * Return: 0
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	{
-		exit(EXIT_FAILURE);
+		return;
+	}
+
+	if (*name == '\0')
+	{
+		return;
 	}
 
 	while (*name != '\0')
 	{
-		f(name);
+		f(*&name);
 		name++;
 	}
 }
+
+
 
