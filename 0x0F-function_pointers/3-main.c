5e6b6e094725538b9846e9f04a57f83e28633b15
lawjr44 <lawsonhazardjunior@gmail.com>
2023-10-11 16:49:12 -0700

    simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int (*op_func)(int, int);
+	int num1, num2;
+	if (argc != 4)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (op_func == NULL)
+	{
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		exit(99);
+	}
+	_putnbr(op_func(num1, num2));
+	_putchar('\n');
+	return (0);
+}
